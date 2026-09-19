0694787f5d227f5f8c0061959adff3beb12dc10d
filipe <felepesilvagalvao@gmail.com>
2026-09-19 15:14:50 -0300

    Testando pipeline

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-  title: "Sistema Localizador de produtos"
+  title: "Localizador de produtos"
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
