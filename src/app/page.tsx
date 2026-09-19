c91292ac5ec91020b75b85bfb9f98c9dea656bae
filipe <felepesilvagalvao@gmail.com>
2026-09-19 14:39:25 -0300

    build: configura static export com basePath para GH Pages e corrige logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Products from "@/components/products/Products";
 import Search from "@/components/search/Search";
 
+const BASE_PATH = "/localizador_de_produtos";
+
 export default function Home() {
   return (
     <main>
       <h1 className="logo">
-        <img src="/logo.png" alt="logo" />
+        <img src={`${BASE_PATH}/logo.png`} alt="logo" />
         <span>Sistema de localização de produtos</span>
       </h1>
       <Search />
