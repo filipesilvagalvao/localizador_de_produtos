313b66bc99e01919a24df99abd3738848c713609
filipe <felepesilvagalvao@gmail.com>
2026-09-19 13:59:25 -0300

    test: bloqueia chamadas reais a fetch nos testes via jest.setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,15 @@
 import "@testing-library/jest-dom";
+
+const realFetch = global.fetch;
+
+beforeEach(() => {
+  global.fetch = jest.fn(() => {
+    throw new Error(
+      "global.fetch foi chamado sem mock. Mocke @/utils/fetchData ou global.fetch no teste."
+    );
+  });
+});
+
+afterAll(() => {
+  global.fetch = realFetch;
+});
